201bd5054ae52422e30a5619dd69dc99ee2580db
Jacqueline <jacquelinemrnbmp@outlook.com>
2024-06-21 01:39:00 -0300

    update server

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 
-const api = axios.create({baseURL : 'http://localhost:3000/api/'});
+const api = axios.create({baseURL : 'http://54.82.164.73/api/'});
 
 export default api
