5905b1b2a5583256abcc2a6b5691f8709883c2b1
Jacqueline <jacquelinemrnbmp@outlook.com>
2024-06-21 10:09:32 -0300

    asd

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 
-const api = axios.create({baseURL : 'http://44.201.246.34:3000/api/'});
+const api = axios.create({baseURL : 'http://44.201.246.34:3000/'});
 
 export default api
